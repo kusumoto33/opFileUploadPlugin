c5c985620043ece04d01c112f51959e3a22762dd
watanabe <watanabe@tejimaya.com>
2012-08-30 21:55:35 +0900

    fixed util helper's name

--- lib/helper/opFileHandleUtilHelper.php
+++ lib/helper/opFileHandleUtilHelper.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * opFileHandleUtilHelper
+ *
+ * @see opUtilHelper#op_include_pager_navigation()
+ */
 function op_file_get_pager_navigation($pager, $internalUri, $options = array())
 {
   $uri = url_for($internalUri);
